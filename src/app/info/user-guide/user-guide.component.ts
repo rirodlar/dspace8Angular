23937022d7296eab60348710bd6d43c0401d7444
ricardo <ricardo.rodriguez.lara@gmail.com>
2026-09-18 00:40:20 -0300

    iframe documents

--- src/app/info/user-guide/user-guide.component.ts
+++ src/app/info/user-guide/user-guide.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeUrlPipe } from '../../shared/utils/safe-url-pipe';
 import { UserGuideContentComponent } from './user-guide-content/user-guide-content.component';
 
 @Component({
@@ -6,10 +7,14 @@ import { UserGuideContentComponent } from './user-guide-content/user-guide-conte
   templateUrl: './user-guide.component.html',
   styleUrls: ['./user-guide.component.scss'],
   standalone: true,
-  imports: [UserGuideContentComponent],
+  imports: [UserGuideContentComponent, SafeUrlPipe],
 })
 /**
  * Componente que muestra la página "Guía de Usuario"
  */
 export class UserGuideComponent {
+  /**
+   * La URL de la guía de usuario externa
+   */
+  userGuideUrl = 'https://sic.usach.cl/docs/';
 }
